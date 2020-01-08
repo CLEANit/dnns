a65bdbb39376cf9de61e86d80a74e3b941bb29be
Kevin Ryczko <kevinryczko@me.com>
2020-01-08 16:53:41 -0500

    reordered some things to get the repo working properly

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='dnns',  
-     version='1.2.6',
+     version='1.2.7',
      scripts=['worker.py'] ,
      author="Kevin Ryczko",
      author_email="kryczko@uottawa.ca",
