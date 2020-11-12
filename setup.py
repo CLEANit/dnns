8fcd873daf932cfdb02f1d7ccb2b114a298a56ea
Kevin Ryczko <kryczko@boltzmann.local>
2020-11-12 09:24:16 -0500

    adding up models module

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='dnns',  
-     version='1.4.5',
+     version='1.4.6',
      scripts=['worker.py', 'lossPlot.py', 'tvpPlot.py', 'splitDataset.py'] ,
      author="Kevin Ryczko",
      author_email="kevin.ryczko@uottawa.ca",
