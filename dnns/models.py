82e4eda126798d6d5336fca4bdff4e4aec49d8e8
kryczko <kryczko@v.cluster.local>
2021-01-18 10:25:03 -0500

    pushing up to work on trixie

--- dnns/models.py
+++ dnns/models.py
@@ -329,6 +329,8 @@ def resnet_3d_152(in_channels, n_classes):
 
 
 def conv3x3x3(in_planes, out_planes, stride=1):
+    print('in channels:', in_planes)
+    print('out channels:', out_planes)
     return nn.Conv3d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=True)
 
 def conv_init(m):
@@ -343,11 +345,12 @@ def conv_init(m):
 class wide_basic(nn.Module):
     def __init__(self, in_planes, planes, dropout_rate, stride=1):
         super(wide_basic, self).__init__()
+        print(in_planes, planes)
         self.bn1 = nn.BatchNorm3d(in_planes)
-        self.conv1 = nn.Conv3d(in_planes, planes, kernel_size=3, padding=1, bias=True)
+        self.conv1 = nn.Conv3d(in_planes, planes, kernel_size=3, padding=1, bias=True, groups=1)
         self.dropout = nn.Dropout(p=dropout_rate)
         self.bn2 = nn.BatchNorm3d(planes)
-        self.conv2 = nn.Conv3d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
+        self.conv2 = nn.Conv3d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True, groups=1)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
@@ -357,14 +360,15 @@ class wide_basic(nn.Module):
 
     def forward(self, x):
         out = self.dropout(self.conv1(F.relu(self.bn1(x))))
-        out = self.conv3d(F.relu(self.bn2(out)))
+        out = self.conv2(F.relu(self.bn2(out)))
         out += self.shortcut(x)
 
         return out
 
 class Wide_ResNet(nn.Module):
-    def __init__(self, depth, widen_factor, dropout_rate, num_classes):
+    def __init__(self, input_size, depth=28, widen_factor=10, dropout_rate=0., num_classes=1):
         super(Wide_ResNet, self).__init__()
+        print(depth)
         self.in_planes = 16
 
         assert ((depth-4)%6 ==0), 'Wide-resnet depth should be 6n+4'
@@ -374,12 +378,12 @@ class Wide_ResNet(nn.Module):
         print('| Wide-Resnet %dx%d' %(depth, k))
         nStages = [16, 16*k, 32*k, 64*k]
 
-        self.conv1 = conv3x3x3(3,nStages[0])
+        self.conv1 = conv3x3x3(4,nStages[0])
         self.layer1 = self._wide_layer(wide_basic, nStages[1], n, dropout_rate, stride=1)
-        self.layer2 = self._wide_layer(wide_basic, nStages[2], n, dropout_rate, stride=1)
+        self.layer2 = self._wide_layer(wide_basic, nStages[2], n, dropout_rate, stride=2)
         self.layer3 = self._wide_layer(wide_basic, nStages[3], n, dropout_rate, stride=1)
         self.bn1 = nn.BatchNorm3d(nStages[3], momentum=0.9)
-        self.linear = nn.Linear(nStages[3], num_classes)
+        self.linear = nn.Linear(640, num_classes)
 
     def _wide_layer(self, block, planes, num_blocks, dropout_rate, stride):
         strides = [stride] + [1]*(int(num_blocks)-1)
@@ -392,6 +396,7 @@ class Wide_ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        x = x.permute(0, 4, 1, 2, 3)
         out = self.conv1(x)
         out = self.layer1(out)
         out = self.layer2(out)
